8459ab5c87a028eb06b9e42e7ab8ca6023ab08ef
alendri <alendri@allbin>
2020-01-30 09:21:02 +0100

    Removed OH dependency.

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,23 +1,14 @@
-import { configure, addParameters } from '@storybook/react';
-import oh from 'output-helpers';
-
-if (!(window as any).has_run) {
-    oh.setConfig({
-        lang: 'sv-SE',
-    });
-
-    (window as any).has_run = true;
-}
+import { configure, addParameters } from "@storybook/react";
 
 // automatically import all files ending in *.stories.tsx
-const req = require.context('../src', true, /\.stories\.tsx$/);
+const req = require.context("../src", true, /\.stories\.tsx$/);
 
 function loadStories() {
-    req.keys().forEach(req);
+  req.keys().forEach(req);
 }
 
 addParameters({
-    backgrounds: [{ name: 'white', value: '#ffffff', default: true }],
+  backgrounds: [{ name: "white", value: "#ffffff", default: true }]
 });
 
 configure(loadStories, module);
