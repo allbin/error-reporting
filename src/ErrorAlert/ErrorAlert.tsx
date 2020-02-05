65050598dc549d58d71378cd2cb88c2d91356028
alendri <alendri@allbin>
2020-02-05 17:01:35 +0100

    Added optional props to control status message.

--- src/ErrorAlert/ErrorAlert.tsx
+++ src/ErrorAlert/ErrorAlert.tsx
@@ -2,21 +2,32 @@ import React, { ReactNode } from "react";
 import { ErrorStatus } from "../";
 import "./style.css";
 
+interface CustomErrorProps {
+  [key: string]: any;
+  body?: string[];
+  /** Message displayed to user while the error report is being completed and sending. Default: "Sending error report.". */
+  report_sending_message?: string;
+  /** Message displayed to user when the error report has successfully been sent. Default: "Error report has been sent.". */
+  report_sent_message?: string;
+  /** Message displayed if the error report could not be completed or sent. Default: "Sending error report failed. Please contact us." */
+  report_failed_message?: string;
+  title?: string;
+  /** The text which appears on the button in the footer of the alert.
+   * Both *action_label* and *actionCB* required to show button.
+   */
+  actionLabel?: string;
+  /** The action triggered when user clicks the button in the footer.
+   * Both *action_label* and *actionCB* required to show button.
+   */
+  actionCB?: () => void;
+}
+
 export interface ErrorAlertProps {
   status?: ErrorStatus | null;
-  custom_error_props?: {
-    [key: string]: any;
-    body?: string[];
-    title?: string;
-    /** The text which appears on the button in the footer of the alert.
-     * Both *action_label* and *actionCB* required to show button.
-     */
-    actionLabel?: string;
-    /** The action triggered when user clicks the button in the footer.
-     * Both *action_label* and *actionCB* required to show button.
-     */
-    actionCB?: () => void;
-  };
+  /** The custom_error_props are available to the Alert component as `props.custom_error_props`.
+   * Any keys may be used, the optional suggested props will be used by the default error alert component, if supplied.
+   */
+  custom_error_props?: CustomErrorProps;
 }
 
 function renderSVGBody(): ReactNode {
@@ -59,6 +70,35 @@ function renderSVGBody(): ReactNode {
       </g>
     </g>
   );
+}
+
+function getStatusMessage(
+  status: ErrorStatus,
+  custom_error_props: CustomErrorProps
+): string {
+  const {
+    report_failed_message,
+    report_sending_message,
+    report_sent_message
+  } = custom_error_props;
+
+  switch (status) {
+    case "sent": {
+      return report_sent_message
+        ? report_sent_message
+        : "Error report has been sent.";
+    }
+    case "failed": {
+      return report_failed_message
+        ? report_failed_message
+        : "Sending error report failed. Please contact us.";
+    }
+    default: {
+      return report_sending_message
+        ? report_sending_message
+        : "Sending error report.";
+    }
+  }
 }
 
 const ErrorAlert: React.FunctionComponent<ErrorAlertProps> = ({
@@ -104,19 +144,20 @@ const ErrorAlert: React.FunctionComponent<ErrorAlertProps> = ({
           </div>
           {body ? (
             <div className="main_body">
-              {body.map(sentence => {
-                return <p key={sentence}>{sentence}</p>;
-              })}
+              {body.map(sentence => (
+                <p key={sentence}>{sentence}</p>
+              ))}
             </div>
           ) : (
             <div className="main_body">
               <p>Something went wrong.</p>
             </div>
           )}
-          {status !== undefined ? (
+          {status ? (
             <div className="status_bar">
-              <b>Error report:</b>{" "}
-              <span className={`status ${status}`}>{status}</span>
+              <span className={`status ${status}`}>
+                {getStatusMessage(status, custom_error_props || {})}
+              </span>
             </div>
           ) : null}
           <div className="actions">
