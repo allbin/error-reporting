f53d170e3488f98060d1151050fd055e21bfcbe4
alendri <alendri@allbin>
2020-10-26 15:12:58 +0100

    Fixed build script.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./test_harness/App";
 import ErrorReporting, { setConfig } from "./module";
 
 setConfig({
